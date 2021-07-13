f05babed94c6380c84550d15a3f82f06f1dbd4e5
Florian Arthofer <florian.arthofer@dynatrace.com>
2021-07-13 10:58:34 +0200

    removed default fallback for networkzone property again, to prevent potential errors when the networkzone feature is not enabled

--- hook.go
+++ hook.go
@@ -318,12 +318,9 @@ func (h *Hook) getDownloadURL(c *credentials) string {
 
 	qv := make(url.Values)
 	qv.Add("bitness", "64")
-	// If no networkzone is provided we set "default" to be safe,
-	// otherwise we'd have an empty "&networkzone=" in the download url
+	// only set the networkzone property when it is configured
 	if c.NetworkZone != "" {
 		qv.Add("networkzone", c.NetworkZone)
-	} else {
-		qv.Add("networkzone", "default")
 	}
 	for _, t := range h.IncludeTechnologies {
 		qv.Add("include", t)
