8c516ad5d4becf3f50a7327279cfb0475a91b92a
Johannes Mittendorfer <johannes.mittendorfer@dynatrace.com>
2024-01-29 13:12:13 +0100

    K8S-8877 Query enableFips

--- hook.go
+++ hook.go
@@ -242,7 +242,7 @@ func (h *Hook) getCredentials() *credentials {
 				CustomOneAgentURL: queryString("customoneagenturl"),
 				SkipErrors:        queryString("skiperrors") == "true",
 				NetworkZone:       queryString("networkzone"),
-				EnableFIPS:        queryString("enabledfips") == "true",
+				EnableFIPS:        queryString("enablefips") == "true",
 			}
 
 			if (creds.EnvironmentID != "" && creds.APIToken != "") || creds.CustomOneAgentURL != "" {
